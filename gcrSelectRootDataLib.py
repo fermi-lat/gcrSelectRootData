22acf0f59e78d0d8a07e78cab1496a7b00c98c43
ecephas <>
2008-12-09 00:08:48 +0000

    Check in gcrSelectRootDataLib.py for gcrSelectRootData v2r2p1 (branch-tag, GlastReleasev15r49).

--- gcrSelectRootDataLib.py
+++ gcrSelectRootDataLib.py
@@ -2,7 +2,8 @@
 
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
-        env.Tool('addLibrary', library = 'gcrSelectRootData')
+        env.Tool('addLibrary', library = ['gcrSelectRootData'])
     env.Tool('commonRootDataLib')
+    env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
     return  1;
