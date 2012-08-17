fd9794dcd6a6fe2150e6ebceff1801efda985815
jrb <>
2012-08-17 18:38:54 +0000

    patches for Windows no-install-headers

--- gcrSelectRootDataLib.py
+++ gcrSelectRootDataLib.py
@@ -3,6 +3,8 @@
 def generate(env, **kw):
     if not kw.get('depsOnly', 0):
         env.Tool('addLibrary', library = ['gcrSelectRootData'])
+        if env['PLATFORM']=='win32' and env.get('CONTAINERNAME','')=='GlastRelease':
+	    env.Tool('findPkgPath', package = 'gcrSelectRootData') 
     env.Tool('commonRootDataLib')
     env.Tool('addLibrary', library = env['rootLibs'])
 def exists(env):
